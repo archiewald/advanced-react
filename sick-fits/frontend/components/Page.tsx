6b0bdadf3f27efd486ce812435addd35e356caef
archiewald <artur.kozubek1@gmail.com>
2018-12-26 18:21:20 +0100

    2.5

--- sick-fits/frontend/components/Page.tsx
+++ sick-fits/frontend/components/Page.tsx
@@ -1,6 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+
 import Header from "./Header";
 import Meta from "./Meta";
+
+const MyButton = styled.button<{ huge?: boolean }>`
+  background: red;
+  font-size: ${props => (props.huge ? "100px" : "50px")};
+  span {
+    font-size: 100px;
+  }
+`;
 
 class Page extends React.Component {
   render() {
@@ -8,6 +18,14 @@ class Page extends React.Component {
       <div>
         <Meta />
         <Header />
+        <MyButton>
+          Click Me
+          <span>💩</span>
+        </MyButton>
+        <MyButton huge>
+          Click Me
+          <span>💩</span>
+        </MyButton>
         {this.props.children}
       </div>
     );
