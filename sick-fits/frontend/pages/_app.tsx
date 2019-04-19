07225b01d7ad639749e0f38757b20f80fd31e2a5
archiewald <artur.kozubek1@gmail.com>
2019-04-19 08:58:25 +0200

    3.15 - Setting up Apollo Client with React

--- sick-fits/frontend/pages/_app.tsx
+++ sick-fits/frontend/pages/_app.tsx
@@ -1,18 +1,43 @@
-import App, { Container } from "next/app";
+import App, { Container, NextAppContext } from "next/app";
 import Page from "../components/Page";
+import { ApolloProvider } from "react-apollo";
+import withData from "../lib/withData";
+import { ApolloClient } from "apollo-boost";
 
-class MyApp extends App {
+interface AppProps {
+  apollo: ApolloClient<{}>;
+}
+
+class MyApp extends App<AppProps> {
+  static async getInitialProps({
+    Component,
+    ctx
+  }: NextAppContext): Promise<{ pageProps: any }> {
+    // correct the types?
+    let pageProps: any = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    // this exposes query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+
   render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
